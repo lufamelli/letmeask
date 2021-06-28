9c71b16c9ace21f27e715193569f756f0cae352d
Lucas Famelli <lucasfamelli@hotmail.com>
2021-06-28 12:03:55 -0300

    configure highlighting and marking questions as answered, sets up automatic depolys with github

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -2,6 +2,9 @@ import{ useHistory, useParams } from 'react-router-dom'
 
 import  deleteImg from '../assets/images/delete.svg'
 import logoImg from '../assets/images/logo.svg';
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg';
+
 import {Button} from '../components/Button';
 import {RoomCode} from '../components/RoomCode';
 //import { useAuth } from '../hooks/useAuth';
@@ -43,6 +46,18 @@ export function AdminRoom() {
         }
     }
 
+    async function handleCheckQuestionAsAnswered(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true,
+        });
+    }
+
+    async function handleHighlightQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true
+        });
+    }
+
     return (
         <div id="page-room">
             <header>
@@ -66,7 +81,28 @@ export function AdminRoom() {
                                 key={question.id} 
                                 content={question.content}
                                 author={question.author}
+                                isAnswered={question.isAnswered}
+                                isHighlighted={question.isHighlighted}
                             >
+
+                                {!question.isAnswered && (
+                                    <>
+                                        <button
+                                            type="button"
+                                            onClick={(() => handleCheckQuestionAsAnswered(question.id))}
+                                        >
+                                            <img src={checkImg} alt="Marcar perginta como pergunta" />
+                                        </button>
+
+                                        <button
+                                            type="button"
+                                            onClick={(() => handleHighlightQuestion(question.id))}
+                                        >
+                                            <img src={answerImg} alt="Dar destaque à pergunta" />
+                                        </button>
+                                    </>
+                                )  }
+
                                 <button
                                     type="button"
                                     onClick={(() => handleDeleteQuestion(question.id))}
